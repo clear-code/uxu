e03bf1efd2880ecb333d10416c6f1ccb06c613c8
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-01-25 02:33:54 +0000

    UxUサーバの自動機能設定を初期状態ではOFFにした
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@91 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- defaults/preferences/uxu.js
+++ defaults/preferences/uxu.js
@@ -1,4 +1,4 @@
-pref("extensions.uxu.auto.start", true);
+pref("extensions.uxu.auto.start", false);
 pref("extensions.uxu.auto.exit",  true);
 pref("extensions.uxu.port",       4444);
 pref("extensions.uxu.mozunit.editor", "/usr/bin/gedit +%l %f");
