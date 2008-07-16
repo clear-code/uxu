fa1d07d1e7815be3a179a02ec1ee445597aa776f
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-07-16 05:38:52 +0000

    subScriptRunner.jsのテスト
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@277 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/unit/subScriptRunner.test.js
@@ -0,0 +1,29 @@
+function setUp()
+{
+}
+
+function tearDown()
+{
+}
+
+testLoad.description = 'スクリプトの中で定義された変数や関数を取り出す';
+function testLoad()
+{
+	var env = {
+			_subScript : [
+				'var a = true;',
+				'this.b = true;',
+				'function c() { return true; }'
+			].join('\n')
+		};
+	var loader = Cc['@mozilla.org/moz/jssubscript-loader;1']
+			.getService(Ci.mozIJSSubScriptLoader);
+	loader.loadSubScript(
+		baseURL + '../../content/uxu/test/helper/subScriptRunner.js',
+		env
+	);
+	assert.isTrue(env.a);
+	assert.isTrue(env.b);
+	assert.isTrue(env.c);
+}
+
