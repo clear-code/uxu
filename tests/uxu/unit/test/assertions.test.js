4e35622fea0dec38b4b861d9edc4516901ee7fdc
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-07-16 16:34:04 +0000

    無駄を省いた
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@306 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/assertions.test.js
+++ tests/uxu/unit/test/assertions.test.js
@@ -10,22 +10,24 @@ function tearDown()
 {
 }
 
-function assertAssertion(aAssertion, aSuccessArgs, aFailureArg)
+assert.assertSucceed = function(aAssertion, aArgs)
 {
-	var done, exception;
-
-	done = false;
+	var done = false;
 	try {
-		aAssertion.apply(assertionsModule, aSuccessArgs);
+		aAssertion.apply(assertionsModule, aArgs);
 		done = true;
 	}
 	catch(e) {
 	}
 	assert.isTrue(done);
+}
 
-	done = false;
+assert.assertFailed = function(aAssertion, aArgs)
+{
+	var exception;
+	var done = false;
 	try {
-		aAssertion.apply(assertionsModule, aFailureArg);
+		aAssertion.apply(assertionsModule, aArgs);
 		done = true;
 	}
 	catch(e) {
@@ -33,63 +35,115 @@ function assertAssertion(aAssertion, aSuccessArgs, aFailureArg)
 	}
 	assert.isFalse(done);
 	assert.equals(exception.name, 'AssertionFailed');
-	assert.isTrue(exception.message.indexOf(aFailureArg[aFailureArg.length-1]) > -1);
+	assert.isTrue(exception.message.indexOf(aArgs[aArgs.length-1]) > -1);
 }
 
 function test_assertions()
 {
 	var message = Math.random() * 65000;
-	assertAssertion(assertionsModule.equals, [1, 1], [0, 1, message]);
-	assertAssertion(assertionsModule.equal, [1, 1], [0, 1, message]);
-	assertAssertion(assertionsModule.notEquals, [0, 1], [1, 1, message]);
-	assertAssertion(assertionsModule.notEqual, [0, 1], [1, 1, message]);
-	assertAssertion(assertionsModule.isTrue, [true], [false, message]);
-	assertAssertion(assertionsModule.isDefined, [true], [void(0), message]);
-	assertAssertion(assertionsModule.isDefined, [false], [void(0), message]);
-	assertAssertion(assertionsModule.isDefined, [0], [void(0), message]);
-	assertAssertion(assertionsModule.isDefined, [null], [void(0), message]);
-	assertAssertion(assertionsModule.isUndefined, [void(0)], [true, message]);
-	assertAssertion(assertionsModule.isUndefined, [void(0)], [false, message]);
-	assertAssertion(assertionsModule.isUndefined, [void(0)], [0, message]);
-	assertAssertion(assertionsModule.isUndefined, [void(0)], [null, message]);
-	assertAssertion(assertionsModule.isNull, [null], [true, message]);
-	assertAssertion(assertionsModule.isNull, [null], [false, message]);
-	assertAssertion(assertionsModule.isNull, [null], [0, message]);
-	assertAssertion(assertionsModule.isNull, [null], [void(0), message]);
-	assertAssertion(assertionsModule.isNotNull, [true], [null, message]);
-	assertAssertion(assertionsModule.isNotNull, [false], [null, message]);
-	assertAssertion(assertionsModule.isNotNull, [0], [null, message]);
-	assertAssertion(assertionsModule.isNotNull, [void(0)], [null, message]);
-	assertAssertion(assertionsModule.raises,
-		['test', function() { throw 'test'; }, {}],
+
+	assert.assertSucceed(assertionsModule.equals, [1, 1]);
+	assert.assertFailed(assertionsModule.equals, [0, 1, message]);
+	assert.assertSucceed(assertionsModule.equal, [1, 1]);
+	assert.assertFailed(assertionsModule.equal, [0, 1, message]);
+
+	assert.assertSucceed(assertionsModule.notEquals, [0, 1]);
+	assert.assertFailed(assertionsModule.notEquals, [1, 1, message]);
+	assert.assertSucceed(assertionsModule.notEqual, [0, 1]);
+	assert.assertFailed(assertionsModule.notEqual, [1, 1, message]);
+
+	assert.assertSucceed(assertionsModule.isTrue, [true]);
+	assert.assertSucceed(assertionsModule.isTrue, [{}]);
+	assert.assertFailed(assertionsModule.isTrue, [false, message]);
+	assert.assertFailed(assertionsModule.isTrue, [0, message]);
+	assert.assertFailed(assertionsModule.isTrue, ['', message]);
+	assert.assertFailed(assertionsModule.isTrue, [null, message]);
+
+	assert.assertSucceed(assertionsModule.isFalse, [false]);
+	assert.assertSucceed(assertionsModule.isFalse, [0]);
+	assert.assertSucceed(assertionsModule.isFalse, ['']);
+	assert.assertSucceed(assertionsModule.isFalse, [null]);
+	assert.assertFailed(assertionsModule.isFalse, [true, message]);
+	assert.assertFailed(assertionsModule.isFalse, [{}, message]);
+
+	assert.assertSucceed(assertionsModule.isDefined, [true]);
+	assert.assertSucceed(assertionsModule.isDefined, [false]);
+	assert.assertSucceed(assertionsModule.isDefined, [0]);
+	assert.assertSucceed(assertionsModule.isDefined, ['']);
+	assert.assertSucceed(assertionsModule.isDefined, [null]);
+	assert.assertFailed(assertionsModule.isDefined, [void(0), message]);
+
+	assert.assertSucceed(assertionsModule.isUndefined, [void(0)]);
+	assert.assertFailed(assertionsModule.isUndefined, [true, message]);
+	assert.assertFailed(assertionsModule.isUndefined, [false, message]);
+	assert.assertFailed(assertionsModule.isUndefined, [0, message]);
+	assert.assertFailed(assertionsModule.isUndefined, ['', message]);
+	assert.assertFailed(assertionsModule.isUndefined, [null, message]);
+
+	assert.assertSucceed(assertionsModule.isNull, [null]);
+	assert.assertFailed(assertionsModule.isNull, [true, message]);
+	assert.assertFailed(assertionsModule.isNull, [false, message]);
+	assert.assertFailed(assertionsModule.isNull, [0, message]);
+	assert.assertFailed(assertionsModule.isNull, ['', message]);
+	assert.assertFailed(assertionsModule.isNull, [void(0), message]);
+
+	assert.assertSucceed(assertionsModule.isNotNull, [true]);
+	assert.assertSucceed(assertionsModule.isNotNull, [false]);
+	assert.assertSucceed(assertionsModule.isNotNull, [0]);
+	assert.assertSucceed(assertionsModule.isNotNull, ['']);
+	assert.assertSucceed(assertionsModule.isNotNull, [void(0)]);
+	assert.assertFailed(assertionsModule.isNotNull, [null, message]);
+
+	assert.assertSucceed(assertionsModule.raises,
+		['test', function() { throw 'test'; }, {}]
+	);
+	assert.assertFailed(assertionsModule.raises,
 		['test', function() { return true; }, {}, message]
 	);
-	assertAssertion(assertionsModule.raise,
-		['test', function() { throw 'test'; }, {}],
+	assert.assertSucceed(assertionsModule.raise,
+		['test', function() { throw 'test'; }, {}]
+	);
+	assert.assertFailed(assertionsModule.raise,
 		['test', function() { return true; }, {}, message]
 	);
-	assertAssertion(assertionsModule.matches,
-		[/te[sx]t/, 'test'],
+
+	assert.assertSucceed(assertionsModule.matches,
+		[/te[sx]t/, 'test']
+	);
+	assert.assertFailed(assertionsModule.matches,
 		[/te[sx]t/, 'tent', message]
 	);
-	assertAssertion(assertionsModule.match,
-		[/te[sx]t/, 'test'],
+	assert.assertSucceed(assertionsModule.match,
+		[/te[sx]t/, 'test']
+	);
+	assert.assertFailed(assertionsModule.match,
 		[/te[sx]t/, 'tent', message]
 	);
-	assertAssertion(assertionsModule.pattern,
-		['test', /te[sx]t/],
+
+	assert.assertSucceed(assertionsModule.pattern,
+		['test', /te[sx]t/]
+	);
+	assert.assertFailed(assertionsModule.pattern,
 		['tent', /te[sx]t/, message]
 	);
-	assertAssertion(assertionsModule.notPattern,
-		['tent', /te[sx]t/],
+
+	assert.assertSucceed(assertionsModule.notPattern,
+		['tent', /te[sx]t/]
+	);
+	assert.assertFailed(assertionsModule.notPattern,
 		['test', /te[sx]t/, message]
 	);
-	assertAssertion(assertionsModule.arrayEquals,
-		[[0, 1, 2], [0, 1, 2]],
+
+	assert.assertSucceed(assertionsModule.arrayEquals,
+		[[0, 1, 2], [0, 1, 2]]
+	);
+	assert.assertFailed(assertionsModule.arrayEquals,
 		[[0, 1, 2], [3, 4, 5], message]
 	);
-	assertAssertion(assertionsModule.arrayEqual,
-		[[0, 1, 2], [0, 1, 2]],
+	assert.assertSucceed(assertionsModule.arrayEqual,
+		[[0, 1, 2], [0, 1, 2]]
+	);
+	assert.assertFailed(assertionsModule.arrayEqual,
 		[[0, 1, 2], [3, 4, 5], message]
 	);
 }
