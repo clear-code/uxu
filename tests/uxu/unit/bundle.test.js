18cf49254a241aa0e323c2e29e83c2a4dc083281
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-07-16 07:32:06 +0000

    bundleのテスト
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@284 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/uxu/unit/bundle.test.js
@@ -0,0 +1,30 @@
+var bundle;
+var locale = utils.getPref('general.useragent.locale');
+
+function setUp()
+{
+	bundle = {};
+	utils.include('../../../content/uxu/lib/bundle.js', bundle);
+}
+
+function tearDown()
+{
+}
+
+function testGetString()
+{
+	var expected = {};
+	if (locale.indexOf('ja') == 0) {
+		expected['report_description_setup'] = 'セットアップ';
+		expected['all_result_failure']       = '1件のテストに失敗しました';
+		expected['assert_equals']            = '"1" が渡されるべき箇所で "2" が渡されました。';
+	}
+	else {
+		expected['report_description_setup'] = 'Setup';
+		expected['all_result_failure']       = '1 test(s) failed.';
+		expected['assert_equals']            = 'Expected "1", got "2".';
+	}
+	assert.equals(expected['report_description_setup'], bundle.getString('report_description_setup'));
+	assert.equals(expected['all_result_failure'], bundle.getFormattedString('all_result_failure', [1]));
+	assert.equals(expected['assert_equals'], bundle.getFormattedString('assert_equals', [1, 2]));
+}
