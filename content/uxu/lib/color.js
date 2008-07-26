b213de51daf263f91f5869c787895c7bc491ef8d
kou <kou@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-07-26 14:10:35 +0000

    * content/uxu/lib/color.js,   tests/uxu/unit/lib/color.test.js: add escape sequence   based color classes.
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@421 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ content/uxu/lib/color.js
@@ -0,0 +1,87 @@
+var NAMES = ["black", "red", "green", "yellow",
+             "blue", "magenta", "cyan", "white"];
+
+function Color(aName, aOptions)
+{
+    this.name = aName;
+    if (!aOptions)
+        aOptions = {};
+
+    this.foreground = aOptions.foreground;
+    if (this.foreground == undefined)
+        this.foreground = true;
+    this.intensity = aOptions.intensity;
+    this.bold = aOptions.bold;
+    this.italic = aOptions.italic;
+    this.underline = aOptions.underline;
+}
+
+Color.prototype = {
+    sequence: function ()
+    {
+        var _sequence = [];
+
+        switch (this.name) {
+            case "none":
+                break;
+            case "reset":
+                _sequence.push("0");
+                break;
+            default:
+                var foregroundParameter, color;
+                foregroundParameter = this.foreground ? 3 : 4;
+                if (this.intensity)
+                    foregroundParameter += 6;
+                color = NAMES.indexOf(this.name);
+                _sequence.push("" + foregroundParameter + color);
+                break;
+        }
+
+        if (this.bold)
+            _sequence.push("1");
+        if (this.italic)
+            _sequence.push("3");
+        if (this.underline)
+            _sequence.push("4");
+
+        return _sequence;
+    },
+
+    escapeSequence: function()
+    {
+        return "\u001b[" + this.sequence().join(";") + "m";
+    },
+
+    concat: function(/* aColor, ... */)
+    {
+        return new MixColor([this].concat(Array.slice(arguments)));
+    }
+};
+
+function MixColor(aColors)
+{
+    this.colors = aColors;
+}
+
+MixColor.prototype = {
+    sequence: function()
+    {
+        var result = [];
+
+        this.colors.forEach(function (aColor) {
+                result = result.concat(aColor.sequence());
+            });
+
+        return result;
+    },
+
+    escapeSequence: function()
+    {
+        return "\u001b[" + this.sequence().join(";") + "m";
+    },
+
+    concat: function(/* aColor, ... */)
+    {
+        return new MixColor([this].concat(Array.slice(arguments)));
+    }
+};
---
+++ tests/uxu/unit/lib/color.test.js
@@ -0,0 +1,45 @@
+var Color;
+var MixColor;
+
+function setUp()
+{
+    var color = {};
+    utils.include('../../../../content/uxu/lib/color.js', color);
+    Color = color.Color;
+    MixColor = color.MixColor;
+}
+
+function testColorEscapeSequence()
+{
+    assertEscapeSequence(["31"], new Color("red"));
+    assertEscapeSequence(["32", "1"], new Color("green", {bold: true}));
+    assertEscapeSequence(["0"], new Color("reset"));
+    assertEscapeSequence(["45"], new Color("magenta", {foreground: false}));
+}
+
+function testMixColorEscapeSequence()
+{
+    assertEscapeSequence(["34", "1"],
+                         new MixColor([new Color("blue"),
+                                       new Color("none", {bold: true})]));
+    var mixColor = new MixColor([new Color("blue"),
+                                 new Color("none", {bold: true})]);
+    assertEscapeSequence(["34", "1", "4"],
+                         mixColor.concat(new Color("none", {underline: true})));
+    assertEscapeSequence(["34", "1", "4"],
+                         new Color("blue").concat(
+                             new Color("none", {bold: true}).concat(
+                                 new Color("none", {underline: true}))));
+}
+
+function assertEscapeSequence(aExpected, aColor)
+{
+    assertEquals(aExpected, aColor.sequence());
+
+    var escapeSequence = aColor.escapeSequence();
+    assertMatch(/\u001b\[(?:\d+;)*\d+m/, escapeSequence);
+
+    var sequenceString;
+    sequenceString = escapeSequence.substring(2, escapeSequence.length - 1);
+    assertEquals(aExpected, sequenceString.split(";"));
+}
