b2c52cb2d90eb0474fc9544ee485b48504f0b5b7
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2008-10-25 19:11:57 +0000

    リモートプロファイルの自動テスト
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@539 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/uxu/functional/remote.test.js
@@ -0,0 +1,17 @@
+var description = '「特定のプロファイルを使用して実行するテスト」のテスト';
+var profile = '../../res/profile/';
+
+function setUp()
+{
+}
+
+function tearDown()
+{
+}
+
+function testProfilePref()
+{
+	var value = utils.getPref('uxu.test.pref.value.boolean');
+	assert.isNotNull(value);
+	assert.isTrue(value);
+}
---
+++ tests/uxu/res/profile/prefs.js
@@ -0,0 +1,1 @@
+user_pref("uxu.test.pref.value.boolean", true);
