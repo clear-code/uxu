2e77865abb04368a17cd3485edba040a06aa1bd2
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-02-03 12:20:23 +0000

    警告の類が出ないようにした
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@686 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/res/profile/prefs.js
+++ tests/uxu/res/profile/prefs.js
@@ -1,3 +1,10 @@
 user_pref("uxu.test.pref.value.boolean", true);
 user_pref("browser.dom.window.dump.enabled", true);
 user_pref("javascript.options.showInConsole", true);
+
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.warnOnQuit", false);
+user_pref("browser.warnOnRestart", false);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
