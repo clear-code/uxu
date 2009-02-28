f3815bcf3709553d59907639515fd086313440cc
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-02-28 16:57:31 +0000

    Thunderbird向けの初期設定
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@777 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/res/profile/prefs.js
+++ tests/uxu/res/profile/prefs.js
@@ -2,9 +2,17 @@ user_pref("uxu.test.pref.value.boolean", true);
 user_pref("browser.dom.window.dump.enabled", true);
 user_pref("javascript.options.showInConsole", true);
 
+// General
+user_pref("extensions.update.enabled", false);
+
+// Firefox
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.tabs.warnOnOpen", false);
 user_pref("browser.warnOnQuit", false);
 user_pref("browser.warnOnRestart", false);
 user_pref("general.warnOnAboutConfig", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
+// Thunderbird
+user_pref("mail.shell.checkDefaultClient", false);
+user_pref("mail.shell.checkDefaultMail", false);
+
