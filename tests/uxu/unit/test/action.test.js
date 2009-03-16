35b68bce48ec3d113ab40e59626e519284b76ebe
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-03-16 16:43:37 +0000

    actionのテストの雛形
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@812 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/uxu/unit/test/action.test.js
@@ -0,0 +1,17 @@
+var topDir = baseURL+'../../../../';
+
+var actionModule;
+
+function setUp()
+{
+	actionModule = {};
+	utils.include(topDir+'content/uxu/test/action.js', actionModule);
+}
+
+function tearDown()
+{
+}
+
+function test()
+{
+}
