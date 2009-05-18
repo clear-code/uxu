ac2148b678fb56969e8b2695533c546c5919a350
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-05-18 04:17:10 +0000

    テスト項目追加（utils.openDatabase(), utils.createDatabase(), utils.createDatabaseFromSQL, utils.createDatabaseFromSQLFile()）
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@946 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/uxu/fixtures/test.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS "test_table";
+CREATE TABLE "test_table" ("key" TEXT PRIMARY KEY  NOT NULL , "value" TEXT);
+INSERT INTO "test_table" VALUES('key1','value1');
+INSERT INTO "test_table" VALUES('key2','value2');
