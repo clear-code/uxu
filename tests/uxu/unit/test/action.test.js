a881f2b5debf0a2860afc09e086ed5ee921158b6
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-06-02 10:11:00 +0000

    テスト追加
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1024 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/action.test.js
+++ tests/uxu/unit/test/action.test.js
@@ -1,6 +1,7 @@
 var topDir = baseURL+'../../../../';
 
 var actionModule;
+var win;
 
 function setUp()
 {
@@ -14,12 +15,55 @@ function tearDown()
 
 /* internal */
 
+test_isFullZoom.tearDown = function() {
+	utils.clearPref('browser.zoom.full');
+};
 function test_isFullZoom()
 {
+	if (utils.checkAppVersion('3.0') < 0) {
+		assert.isFalse(actionModule.isFullZoom());
+	}
+	else {
+		utils.setPref('browser.zoom.full', false);
+		assert.isFalse(actionModule.isFullZoom());
+		utils.setPref('browser.zoom.full', true);
+		assert.isTrue(actionModule.isFullZoom());
+		utils.setPref('browser.zoom.full', false);
+		assert.isFalse(actionModule.isFullZoom());
+	}
 }
 
+test_getZoom.shouldSkip = utils.checkAppVersion('3.0') < 0;
+test_getZoom.setUp = function() {
+	utils.setPref('browser.zoom.full', true);
+	yield Do(utils.setUpTestWindow('about:'));
+	win = utils.getTestWindow();
+};
+test_getZoom.tearDown = function() {
+	win.FullZoom.reset();
+	win = null;
+	utils.tearDownTestWindow();
+};
 function test_getZoom()
 {
+	var zoom = utils.getPref('toolkit.zoomManager.zoomValues').split(',').map(parseFloat);
+	var index = zoom.indexOf(1);
+
+	win.FullZoom.reset();
+	win.FullZoom.enlarge();
+	win.FullZoom.enlarge();
+	assert.equals(
+		Math.floor(zoom[index+2] * 100),
+		Math.floor(actionModule.getZoom(content) * 100)
+	);
+
+	win.FullZoom.reset();
+	win.FullZoom.reduce();
+	win.FullZoom.reduce();
+	assert.equals(
+		Math.floor(zoom[index-2] * 100),
+		Math.floor(actionModule.getZoom(content) * 100)
+	);
 }
 
 /* public */
