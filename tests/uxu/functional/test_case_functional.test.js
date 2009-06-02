51f82c5d4baa2ede7c146f1acff89b3b4ddcf29f
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-06-02 02:39:38 +0000

    テストケースとサーバ機能の結合テスト
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1001 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ tests/uxu/functional/test_case_functional.test.js
@@ -0,0 +1,43 @@
+var topDir = baseURL+'../../../';
+
+utils.include(topDir+'content/uxu/lib/module_manager.js');
+
+var test_module = new ModuleManager([topDir+'content/uxu/test']);
+var TestCaseClass = test_module.require('class', 'test_case');
+var EnvironmentClass = test_module.require('class', 'environment');
+
+var testcase;
+
+function setUp()
+{
+	testcase = new TestCaseClass('description');
+	testcase.environment = new EnvironmentClass({}, baseURL, gBrowser);
+	yield 0; // to run tests progressively
+}
+
+function tearDown()
+{
+}
+
+test_testCaseWithHttpDaemons.assertions = 7;
+function test_testCaseWithHttpDaemons()
+{
+	var base = baseURL+'../fixtures/';
+	testcase.tests = {
+		'1' : function() {
+			assert.isFalse(serverUtils.isHttpServerRunning());
+			var port = 4445;
+			yield Do(utils.setUpHttpServer(port, base));
+			yield Do(utils.loadURI('http://localhost:'+port+'/html.html'));
+			assert.equals('test', content.document.title);
+			assert.isTrue(serverUtils.isHttpServerRunning());
+		}
+	};
+	assert.equals(1, testcase.tests.length);
+	testcase.masterPriority = 'must';
+	assert.isFalse(testcase.done);
+	testcase.run();
+	yield 500;
+	assert.isTrue(testcase.done);
+	assert.isFalse(testcase.environment.serverUtils.isHttpServerRunning());
+}
