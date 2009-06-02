7ed263ef6aa65210fd55b87bb5d3be63febfa86f
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-06-02 09:47:39 +0000

    テスト追加の準備
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1022 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/action.test.js
+++ tests/uxu/unit/test/action.test.js
@@ -12,6 +12,46 @@ function tearDown()
 {
 }
 
-function test()
+/* internal */
+
+function test_isFullZoom()
+{
+}
+
+function test_getZoom()
+{
+}
+
+/* public */
+
+function test_fireMouseEvent()
+{
+}
+
+function test_fireMouseEventOnElement()
+{
+}
+
+function test_fireKeyEventOnElement()
+{
+}
+
+function test_inputTextToField()
+{
+}
+
+function test_getElementFromScreenPoint()
+{
+}
+
+function test_getWindowFromScreenPoint()
+{
+}
+
+function test_fireXULCommandEvent()
+{
+}
+
+function test_fireXULCommandEventOnElement()
 {
 }
