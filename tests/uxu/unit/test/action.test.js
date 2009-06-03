dccf28911277e3eb267973705368956228a33da7
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-06-03 05:06:43 +0000

    テスト追加
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1035 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/action.test.js
+++ tests/uxu/unit/test/action.test.js
@@ -431,6 +431,74 @@ function test_inputTextToField()
 
 function test_getElementFromScreenPoint()
 {
+	var target = $('clickable-box'),
+		targetBoxObject,
+		rootBoxObject = gBrowser.boxObject;
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			content,
+			rootBoxObject.screenX + 50,
+			rootBoxObject.screenY + 50
+		)
+	);
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			gBrowser.ownerDocument.defaultView,
+			rootBoxObject.screenX + 50,
+			rootBoxObject.screenY + 50
+		)
+	);
+	assert.isNull(
+		actionModule.getElementFromScreenPoint(
+			content,
+			rootBoxObject.screenX - 150,
+			rootBoxObject.screenY - 150
+		)
+	);
+
+	yield Do(utils.loadURI(topDir+'tests/uxu/fixtures/frameTest.html'));
+	rootBoxObject = utils.getBoxObjectFor($('frame3'));
+	target = $('frame3').contentDocument.getElementById('em1');
+	targetBoxObject = utils.getBoxObjectFor(target);
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			content,
+			targetBoxObject.screenX + 5,
+			targetBoxObject.screenY + 5
+		)
+	);
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			gBrowser.ownerDocument.defaultView,
+			targetBoxObject.screenX + 5,
+			targetBoxObject.screenY + 5
+		)
+	);
+
+	yield Do(utils.loadURI(topDir+'tests/uxu/fixtures/frameTestInline.html'));
+	rootBoxObject = utils.getBoxObjectFor($('frame2'));
+	target = $('frame2').contentDocument.getElementById('em1');
+	targetBoxObject = utils.getBoxObjectFor(target);
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			content,
+			targetBoxObject.screenX + 5,
+			targetBoxObject.screenY + 5
+		)
+	);
+	assert.equals(
+		target,
+		actionModule.getElementFromScreenPoint(
+			gBrowser.ownerDocument.defaultView,
+			targetBoxObject.screenX + 5,
+			targetBoxObject.screenY + 5
+		)
+	);
 }
 
 function test_getWindowFromScreenPoint()
@@ -461,6 +529,25 @@ function test_getWindowFromScreenPoint()
 	);
 
 	yield Do(utils.loadURI(topDir+'tests/uxu/fixtures/frameTest.html'));
+	rootBoxObject = utils.getBoxObjectFor($('frame3'));
+	assert.equals(
+		$('frame3').contentWindow,
+		actionModule.getWindowFromScreenPoint(
+			content,
+			rootBoxObject.screenX + 50,
+			rootBoxObject.screenY + 50
+		)
+	);
+	assert.equals(
+		$('frame3').contentWindow,
+		actionModule.getWindowFromScreenPoint(
+			gBrowser.ownerDocument.defaultView,
+			rootBoxObject.screenX + 50,
+			rootBoxObject.screenY + 50
+		)
+	);
+
+	yield Do(utils.loadURI(topDir+'tests/uxu/fixtures/frameTestInline.html'));
 	rootBoxObject = utils.getBoxObjectFor($('frame2'));
 	assert.equals(
 		$('frame2').contentWindow,
@@ -468,15 +555,7 @@ function test_getWindowFromScreenPoint()
 			content,
 			rootBoxObject.screenX + 50,
 			rootBoxObject.screenY + 50
-		),
-		[
-			'expected: '+$('frame2').contentWindow.location.href,
-			'actual: '+actionModule.getWindowFromScreenPoint(
-				content,
-				rootBoxObject.screenX + 50,
-				rootBoxObject.screenY + 50
-			).location.href
-		].join('\n')
+		)
 	);
 	assert.equals(
 		$('frame2').contentWindow,
