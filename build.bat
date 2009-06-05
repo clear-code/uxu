8700e07b64ba4c5883bcfb9a7957ae2bc4601318
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-06-05 12:31:55 +0000

    change filename of buildscript
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1074 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

---
+++ build.bat
@@ -0,0 +1,3 @@
+copy buildscript\make_new.sh .\
+bash make_new.sh uxu version=1
+del make_new.sh
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp buildscript/make_new.sh ./
+./make_new.sh uxu version=1
+rm make_new.sh
