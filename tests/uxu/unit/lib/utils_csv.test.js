4074051db24367928e3f8b1a6b853b5e1239138a
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-10-15 18:48:16 +0000

    テンプレートを使ったテスト
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1143 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/lib/utils_csv.test.js
+++ tests/uxu/unit/lib/utils_csv.test.js
@@ -3,6 +3,7 @@
 utils.include('utils_common.inc.js');
 
 var csvBase = topDir+'tests/uxu/fixtures/csv/';
+var values = { a: 29, b: true, c: "string" };
 var CSVParameters = {
 		simple : { source    : utils.readFrom(csvBase+'simple.csv', 'UTF-8'),
 		           CSV       : utils.readJSON(csvBase+'simple.csv.json', 'UTF-8'),
@@ -12,7 +13,10 @@ var CSVParameters = {
 		           parameter : utils.readJSON(csvBase+'hash.parameter.json', 'UTF-8') },
 		typed  : { source    : utils.readFrom(csvBase+'typed.csv', 'UTF-8'),
 		           CSV       : utils.readJSON(csvBase+'typed.csv.json', 'UTF-8'),
-		           parameter : utils.readJSON(csvBase+'typed.parameter.json', 'UTF-8') }
+		           parameter : utils.readJSON(csvBase+'typed.parameter.json', 'UTF-8') },
+		template : { source    : utils.parseTemplate(utils.readFrom(csvBase+'template.csv', 'UTF-8'), values),
+		             CSV       : utils.readJSON(csvBase+'template.csv.json', 'UTF-8', values),
+		             parameter : utils.readJSON(csvBase+'template.parameter.json', 'UTF-8', values) }
 	};
 
 test_parseCSV.parameters = CSVParameters;
