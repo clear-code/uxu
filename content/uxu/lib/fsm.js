de3480e5c824223a5b5e4a95675f9661a83ca9ad
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2009-10-20 09:39:31 +0000

    期待通りに働いていなかった
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1187 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- content/uxu/lib/fsm.js
+++ content/uxu/lib/fsm.js
@@ -1,14 +1,13 @@
 // from MozLab
-function go(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers)
+var MAX_NEST = 50;
+function go(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers, aNest)
 {
+	if (!aNest) aNest = { count : 0 };
 	if (!aEventHandlers) aEventHandlers = {};
 	if ('state/enter' in aEventHandlers)
 		aEventHandlers['state/enter'].forEach(function(aEventHandler) {
 			aEventHandler.call(aContext, aStateName);
 		});
-
-	var nest = 0;
-	var MAX_NEST = 50;
 
 	aStateHandlers[aStateName].call(
 		aContext,
@@ -21,8 +20,8 @@ function go(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandl
 
 			var nextState = aStateTransitions[aStateName][aExitResult];
 			if (nextState) {
-				if (++nest < MAX_NEST)
-					go(nextState, aContext, aStateHandlers, aStateTransitions, aEventHandlers);
+				if (++aNest.count < MAX_NEST)
+					go(nextState, aContext, aStateHandlers, aStateTransitions, aEventHandlers, aNest);
 				else
 					window.setTimeout(go, 0, nextState, aContext, aStateHandlers, aStateTransitions, aEventHandlers);
 			}
