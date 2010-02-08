5584f0c95554a6110d43d204f569c137affc41fd
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2010-02-08 17:36:23 +0000

    関数のネストが深くならないようにした
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1280 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- content/uxu/lib/fsm.js
+++ content/uxu/lib/fsm.js
@@ -1,33 +1,65 @@
 // from MozLab
-var MAX_NEST = 100;
-function go(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers, aNest)
+function go(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers)
 {
-	if (!aNest) aNest = { count : 0 };
-	if (!aEventHandlers) aEventHandlers = {};
-	if ('state/enter' in aEventHandlers)
-		aEventHandlers['state/enter'].forEach(function(aEventHandler) {
-			aEventHandler.call(aContext, aStateName);
-		});
-
-	aStateHandlers[aStateName].call(
-		aContext,
-		function(aExitResult)
+	function generate(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers)
+	{
+		if (!aEventHandlers) aEventHandlers = {};
+		var stateName = aStateName;
+		var start = Date.now();
+		while (true)
 		{
-			if ('state/exit' in aEventHandlers)
-				aEventHandlers['state/exit'].forEach(function(aEventHandler) {
-					aEventHandler.call(aContext, aStateName);
+			if ('state/enter' in aEventHandlers)
+				aEventHandlers['state/enter'].forEach(function(aEventHandler) {
+					aEventHandler.call(aContext, stateName);
 				});
 
-			var nextState = aStateTransitions[aStateName][aExitResult];
-			if (nextState) {
-				if (++aNest.count < MAX_NEST) {
-					go(nextState, aContext, aStateHandlers, aStateTransitions, aEventHandlers, aNest);
+			let exitResult = null;
+			aStateHandlers[stateName].call(
+				aContext,
+				function(aExitResult)
+				{
+					if ('state/exit' in aEventHandlers)
+						aEventHandlers['state/exit'].forEach(function(aEventHandler) {
+							aEventHandler.call(aContext, stateName);
+						});
+
+					exitResult = aExitResult;
 				}
-				else {
-					aNest.count = 0;
-					window.setTimeout(go, 0, nextState, aContext, aStateHandlers, aStateTransitions, aEventHandlers, aNest);
-				}
+			);
+
+			// continuation関数が実行されるまで待つ
+			while (!exitResult)
+			{
+				yield;
 			}
+
+			// 一定以上の時間が経過したら強制的にループを切る。
+			// 長時間ループが回り続けると、Firefox自身が警告を表示してしまう。
+			let now = Date.now();
+			if (now - start >= 5000) {
+				start = now;
+				yield;
+			}
+
+			stateName = aStateTransitions[stateName][exitResult];
+			if (!stateName) // 全stateを通過し終えたので、終了
+				break;
 		}
-	);
+	}
+
+	var iterator = generate(aStateName, aContext, aStateHandlers, aStateTransitions, aEventHandlers);
+	try {
+		iterator.next();
+		var timer = window.setInterval(function() {
+				try {
+					iterator.next();
+				}
+				catch(e if e instanceof StopIteration) {
+					window.clearInterval(timer);
+				}
+			}, 1);
+	}
+	catch(e if e instanceof StopIteration) {
+		// finished
+	}
 }
