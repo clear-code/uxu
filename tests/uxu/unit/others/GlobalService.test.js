69c9d3a21479a2cbc47109a0097dea407c1d9f75
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2010-03-23 02:47:25 +0000

    不要になったテストコードが残っていたのを削除
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1298 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/others/GlobalService.test.js
+++ tests/uxu/unit/others/GlobalService.test.js
@@ -11,30 +11,7 @@ function tearDown()
 {
 }
 
-function testGetInstallLocation()
+function testFoo()
 {
-	var dir = utils.getFileFromKeyword('CurProcD');
-	dir.append('extensions');
-	dir.append('uxu@clear-code.com');
-	assert.equals(dir.path, service.globalLocation.path);
-
-	var id = 'uxu@clear-code.com';
-	dir = Cc['@mozilla.org/extensions/manager;1']
-			.getService(Ci.nsIExtensionManager)
-			.getInstallLocation(id)
-			.getItemLocation(id);
-	assert.equals(dir.path, service.installedLocation.path);
-}
-
-function testGetVersion()
-{
-	var file = utils.getFileFromURLSpec(baseURL+'../../fixtures/element.install.rdf');
-	assert.isNotNull(file);
-	var version = service.getVersionFromManifest(file);
-	assert.equals('0.12.3.45', version);
-
-	file = utils.getFileFromURLSpec(baseURL+'../../fixtures/attribute.install.rdf');
-	assert.isNotNull(file);
-	version = service.getVersionFromManifest(file);
-	assert.equals('0.12.3.45', version);
+	assert.isTrue(true);
 }
