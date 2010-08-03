3bf2392958c8ed5d78786ac862d0c6d753f8c54b
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2010-08-03 04:23:56 +0000

    readyTo...系のテストを追加
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1459 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/action.test.js
+++ tests/uxu/unit/test/action.test.js
@@ -2,8 +2,12 @@ var topDir = baseURL+'../../../../';
 
 var Action = utils.import(topDir+'modules/test/action.js', {}).Action;
 
+var PromptService = Cc['@mozilla.org/embedcomp/prompt-service;1']
+					.getService(Ci.nsIPromptService);
 
 var actionModule;
+
+var ACCEPT_DELAY = 1000;
 
 function setUp()
 {
@@ -18,32 +22,200 @@ function tearDown()
 
 function test_readyToOK()
 {
-	actionModule.readyToOK();
-	var before = Date.now();
-	alert('click OK');
-	assert.compare(1000, '>', Date.now() - before);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToOK();
+			alert('click OK');
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToOK();
+			PromptService.alert(null, 'title', 'click OK');
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToOK({ checked : true });
+			var checked = {};
+			PromptService.alertCheck(null, 'title', 'click OK', 'check', checked);
+			assert.isTrue(checked.value);
+		}
+	);
 }
 
 function test_readyToConfirm()
 {
-	var before = Date.now();
-	actionModule.readyToConfirm(true);
-	var result = confirm('click OK');
-	assert.isTrue(result);
-	assert.compare(1000, '>', Date.now() - before);
-
-	before = Date.now();
-	actionModule.readyToConfirm(false);
-	result = confirm('click Cancel');
-	assert.isFalse(result);
-	assert.compare(1000, '>', Date.now() - before);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(true);
+			var result = confirm('click OK');
+			assert.isTrue(result);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(false);
+			var result = confirm('click Cancel');
+			assert.isFalse(result);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(true);
+			var result = PromptService.confirm(null, 'title', 'click OK');
+			assert.isTrue(result);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(false);
+			var result = PromptService.confirm(null, 'title', 'click Cancel');
+			assert.isFalse(result);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(true, { checked : true });
+			var checked = {};
+			var result = PromptService.confirmCheck(null, 'title', 'click OK', 'check', checked);
+			assert.isTrue(result);
+			assert.isTrue(checked.value);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(false, { checked : true });
+			var checked = {};
+			var result = PromptService.confirmCheck(null, 'title', 'click Cancel', 'check', checked);
+			assert.isFalse(result);
+			assert.isTrue(checked.value);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(0, { checked : true });
+			var checked = {};
+			var result = PromptService.confirmEx(null, 'title', 'text', 
+							(PromptService.BUTTON_POS_0 * PromptService.BUTTON_TITLE_SAVE) |
+							(PromptService.BUTTON_POS_1 * PromptService.BUTTON_TITLE_DONT_SAVE) |
+							(PromptService.BUTTON_POS_2 * PromptService.BUTTON_TITLE_CANCEL),
+							null, null, null,
+							'check', checked);
+			assert.equals(0, result);
+			assert.isTrue(checked.value);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(1, { checked : true });
+			var checked = {};
+			var result = PromptService.confirmEx(null, 'title', 'text', 
+							(PromptService.BUTTON_POS_0 * PromptService.BUTTON_TITLE_SAVE) |
+							(PromptService.BUTTON_POS_1 * PromptService.BUTTON_TITLE_DONT_SAVE) |
+							(PromptService.BUTTON_POS_2 * PromptService.BUTTON_TITLE_CANCEL),
+							null, null, null,
+							'check', checked);
+			assert.equals(1, result);
+			assert.isTrue(checked.value);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToConfirm(2, { checked : true });
+			var checked = {};
+			var result = PromptService.confirmEx(null, 'title', 'text', 
+							(PromptService.BUTTON_POS_0 * PromptService.BUTTON_TITLE_SAVE) |
+							(PromptService.BUTTON_POS_1 * PromptService.BUTTON_TITLE_DONT_SAVE) |
+							(PromptService.BUTTON_POS_2 * PromptService.BUTTON_TITLE_CANCEL),
+							null, null, null,
+							'check', checked);
+			assert.equals(2, result);
+			assert.isTrue(checked.value);
+		}
+	);
 }
 
 function test_readyToPrompt()
 {
-	var before = Date.now();
-	actionModule.readyToPrompt('input');
-	var result = prompt('test');
-	assert.equals('input', result);
-	assert.compare(1000, '>', Date.now() - before);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToPrompt('input');
+			var result = prompt('test');
+			assert.equals('input', result);
+		}
+	);
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToPrompt('input', { checked : true });
+			var input = {};
+			var checked = {};
+			var result = PromptService.prompt(null, 'title', 'test', input, 'check', checked);
+			assert.isTrue(result);
+			assert.equals('input', input.value);
+			assert.isTrue(checked.value);
+		}
+	);
+}
+
+function test_readyToPromptPassword()
+{
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToPromptPassword('password', { checked : true });
+			var password = {};
+			var checked = {};
+			var result = PromptService.promptPassword(null, 'title', 'text', password, 'check', checked);
+			assert.isTrue(result);
+			assert.equals('password', password.value);
+			assert.isTrue(checked.value);
+		}
+	);
+}
+
+function test_readyToPromptUsernameAndPassword()
+{
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToPromptUsernameAndPassword('username', 'password', { checked : true });
+			var username = {};
+			var password = {};
+			var checked = {};
+			var result = PromptService.promptUsernameAndPassword(null, 'title', 'text', username, password, 'check', checked);
+			assert.isTrue(result);
+			assert.equals('username', username.value);
+			assert.equals('password', password.value);
+			assert.isTrue(checked.value);
+		}
+	);
+}
+
+function test_readyToSelect()
+{
+	assert.finishWithin(
+		ACCEPT_DELAY,
+		function() {
+			actionModule.readyToSelect(1);
+			var selection = {};
+			var result = PromptService.select(null, 'title', 'text', 3, ['a', 'b', 'c'], selection);
+			assert.isTrue(result);
+			assert.equals(1, selection.value);
+		}
+	);
 }
