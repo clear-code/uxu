e870ad3d031fd8b9029f84443b1623e3cbc4f930
piro <piro@5c92f73f-0fc2-db11-88f6-0010dcdd6dc2>
2010-08-12 12:26:12 +0000

    テスト追加
    git-svn-id: http://www.cozmixng.org/repos/mozilla/uxu/trunk@1546 5c92f73f-0fc2-db11-88f6-0010dcdd6dc2

--- tests/uxu/unit/test/suite.test.js
+++ tests/uxu/unit/test/suite.test.js
@@ -365,3 +365,18 @@ function test_addRemoveWindowWatcher_object(aParameter)
 	watcher = null;
 }
 
+
+function test_JsMockitoStyleMock()
+{
+	var mock = mockFunction('mock function');
+	when(mock)(10, 100).thenReturn(1000);
+	assert.equals(1000, mock(10, 100));
+}
+
+function test_JSMockStyleMock()
+{
+	var controller = MockControl();
+	var mock = controller.createMock();
+	mock.expects().myMethod(10, 100).andReturn(1000);
+	assert.equals(1000, mock.myMethod(10, 100));
+}
