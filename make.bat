1cd77c7e4e05f0b2a0478d2f431e82fdbb3f813d
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-04-03 12:59:00 +0900

    Add build script

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-copy buildscript\make_new.sh .\
-bash make_new.sh uxu version=1
-del make_new.sh
+copy buildscript\makexpi.sh .\
+bash makexpi.sh -n uxu -v
+del makexpi.sh
--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp buildscript/make_new.sh ./
-./make_new.sh uxu version=1
-rm make_new.sh
+cp buildscript/makexpi.sh ./
+./makexpi.sh -n uxu -v
+rm makexpi.sh
